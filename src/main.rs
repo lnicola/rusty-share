47db4efcb05a280a9e3e15fdac9477834a03b7a4
Laurentiu Nicola <lnicola@dend.ro>
2017-11-13 23:01:50 +0200

    Implement archive support

--- src/main.rs
+++ src/main.rs
@@ -7,32 +7,116 @@ extern crate futures_cpupool;
 extern crate futures_fs;
 extern crate hyper;
 extern crate pretty_env_logger;
+extern crate tar;
+extern crate tokio_core;
 extern crate url;
+extern crate walkdir;
 
 use bytes::Bytes;
 use bytesize::ByteSize;
 use chrono::{DateTime, Local, TimeZone};
 use chrono_humanize::HumanTime;
-use futures::future;
-use futures::{stream, Future, Sink, Stream};
+use futures::{future, stream, Future, Sink, Stream};
+use futures::sync::mpsc;
 use futures_cpupool::CpuPool;
 use futures_fs::FsPool;
 use hyper::{Get, Post, StatusCode};
-use hyper::header::{ContentLength, Location};
+use hyper::header::{Charset, ContentDisposition, ContentLength, ContentType, DispositionParam,
+                    DispositionType, Location};
+use hyper::mime::{Mime, TEXT_HTML_UTF_8};
 use hyper::server::{Http, Request, Response, Service};
+use std::error;
 use std::ffi::OsString;
-use std::fs;
+use std::fs::{self, File};
 use std::io;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::fmt::Write;
 use std::sync::Arc;
 use std::time::UNIX_EPOCH;
-use url::percent_encoding;
+use tar::Builder;
+use tokio_core::reactor::{Core, Handle};
+use url::{form_urlencoded, percent_encoding};
+use walkdir::{DirEntry, WalkDir};
+
+struct Pipe {
+    dest: futures::sink::Wait<futures::sync::mpsc::Sender<Bytes>>,
+}
+
+impl io::Write for Pipe {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        match self.dest.send(Bytes::from(buf)) {
+            Ok(_) => Ok(buf.len()),
+            Err(e) => Err(io::Error::new(io::ErrorKind::Interrupted, e)),
+        }
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        match self.dest.flush() {
+            Ok(_) => Ok(()),
+            Err(e) => Err(io::Error::new(io::ErrorKind::Interrupted, e)),
+        }
+    }
+}
+
+struct Archiver {
+    root: PathBuf,
+}
+
+impl Archiver {
+    fn write_entry<W>(
+        &self,
+        entry: &DirEntry,
+        builder: &mut Builder<W>,
+    ) -> std::result::Result<(), Box<error::Error>>
+    where
+        W: io::Write,
+    {
+        let relative_path = try!(entry.path().strip_prefix(&self.root));
+        let metadata = try!(entry.metadata());
+        if metadata.is_dir() {
+            try!(builder.append_dir(&relative_path, &entry.path()));
+        } else {
+            let mut file = try!(File::open(&entry.path()));
+            try!(builder.append_file(&relative_path, &mut file));
+        }
+
+        Ok(())
+    }
+
+    fn add_to_archive<P, W>(&self, entry: P, builder: &mut Builder<W>) -> std::io::Result<()>
+    where
+        P: AsRef<Path>,
+        W: io::Write,
+    {
+        let walkdir = WalkDir::new(&self.root.join(&entry));
+        let entries = walkdir
+            .into_iter()
+            .filter_entry(|e| !Archiver::is_hidden(e));
+        for e in entries {
+            if let Ok(e) = e {
+                if let Err(e) = self.write_entry(&e, builder) {
+                    println!("{:?}", e);
+                }
+            } else {
+                println!("{:?}", e);
+            }
+        }
+        Ok(())
+    }
+
+    fn is_hidden(entry: &DirEntry) -> bool {
+        entry
+            .file_name()
+            .to_str()
+            .map_or(true, |s| s.starts_with('.'))
+    }
+}
 
 struct Server {
-    pool: CpuPool,
+    handle: Handle,
     fs_pool: Arc<FsPool>,
+    pool: Arc<CpuPool>,
 }
 
 impl Service for Server {
@@ -45,8 +129,9 @@ impl Service for Server {
         type Body = Box<Stream<Item = Bytes, Error = hyper::Error> + Send>;
 
         let root = Path::new(".");
-        let response = match (req.method(), req.path()) {
-            (&Get, path_) => {
+        let response = match *req.method() {
+            Get => {
+                let path_ = req.path();
                 let path_ = percent_encoding::percent_decode(path_.as_bytes())
                     .decode_utf8()
                     .unwrap()
@@ -61,8 +146,8 @@ impl Service for Server {
                 } else {
                     let metadata = metadata.unwrap();
                     if metadata.is_dir() {
-                        if !path_.ends_with('/') &&
-                            fs::symlink_metadata(&path)
+                        if !path_.ends_with('/')
+                            && fs::symlink_metadata(&path)
                                 .unwrap()
                                 .file_type()
                                 .is_symlink()
@@ -78,6 +163,7 @@ impl Service for Server {
                             let body = Box::new(stream::once(Ok(bytes))) as Body;
                             let response = Response::new()
                                 .with_status(StatusCode::Ok)
+                                .with_header(ContentType(TEXT_HTML_UTF_8))
                                 .with_header(ContentLength(len))
                                 .with_body(body);
 
@@ -94,18 +180,51 @@ impl Service for Server {
                     }
                 }
             }
-            (&Post, _path) => {
-                let (tx, rx) = futures::sync::mpsc::channel::<Bytes>(10);
-                let r = self.pool.spawn_fn(|| tx.send(Bytes::from("hello"))).then(
-                    |_| {
-                        let response =
-                            Response::new()
-                                .with_status(StatusCode::Ok)
-                                .with_body(Box::new(rx.map_err(|_| hyper::Error::Method)) as Body);
-                        Box::new(future::ok(response))
-                    },
-                );
-                return Box::new(r);
+            Post => {
+                let pool = Arc::clone(&self.pool);
+                let handle = self.handle.clone();
+                let b = req.body().concat2().and_then(move |b| {
+                    use futures::future::FutureResult;
+                    let (tx, rx) = mpsc::channel::<Bytes>(10);
+
+                    let f = pool.spawn_fn(move || -> FutureResult<_, ()> {
+                        let pipe = Pipe { dest: tx.wait() };
+
+                        let mut a = Builder::new(pipe);
+                        let archiver = Archiver {
+                            root: PathBuf::from("."),
+                        };
+
+                        for (name, value) in form_urlencoded::parse(&b) {
+                            if name == "selection[]" {
+                                archiver
+                                    .add_to_archive(Path::new(value.as_ref()), &mut a)
+                                    .unwrap();
+                            }
+                        }
+
+
+                        future::ok(())
+                    });
+                    handle.spawn(f);
+
+                    Ok(
+                        Response::new()
+                            .with_header(ContentDisposition {
+                                disposition: DispositionType::Attachment,
+                                parameters: vec![
+                                    DispositionParam::Filename(
+                                        Charset::Iso_8859_1,
+                                        None,
+                                        b"archive.tar".to_vec(),
+                                    ),
+                                ],
+                            })
+                            .with_header(ContentType("application/x-tar".parse::<Mime>().unwrap()))
+                            .with_body(Box::new(rx.map_err(|_| hyper::Error::Incomplete)) as Body),
+                    )
+                });
+                return Box::new(b);
             }
             _ => {
                 let response = Response::new().with_status(StatusCode::InternalServerError);
@@ -121,19 +240,37 @@ fn main() {
     pretty_env_logger::init().unwrap();
     let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 3010);
 
+    let mut core = Core::new().unwrap();
+    let handle = core.handle();
+
+    let handle1 = handle.clone();
     let server = Http::new()
-        .bind(&addr, || {
+        .serve_addr_handle(&addr, &handle, move || {
             Ok(Server {
-                pool: CpuPool::new_num_cpus(),
+                handle: handle1.clone(),
                 fs_pool: Arc::new(FsPool::new(4)),
+                pool: Arc::new(CpuPool::new_num_cpus()),
             })
         })
         .unwrap();
+
     println!(
         "Listening on http://{} with 1 thread.",
-        server.local_addr().unwrap()
+        server.incoming_ref().local_addr()
     );
-    server.run().unwrap();
+
+
+    let handle1 = handle.clone();
+    handle.spawn(
+        server
+            .for_each(move |conn| {
+                handle1.spawn(conn.map(|_| ()).map_err(|err| println!("error: {:?}", err)));
+                Ok(())
+            })
+            .map_err(|_| ()),
+    );
+
+    core.run(future::empty::<(), ()>()).unwrap();
 }
 
 #[derive(Debug)]
