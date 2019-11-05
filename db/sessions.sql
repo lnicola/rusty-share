e4f8b29cd33eeb6d62fb10c268497531276113d1
Laurențiu Nicola <lnicola@dend.ro>
2019-11-06 00:39:27 +0200

    Fix table name in db script

--- db/sessions.sql
+++ db/sessions.sql
@@ -1,6 +1,6 @@
 create table sessions (
     id blob not null primary key,
-    user_id integer not null references user(id),
+    user_id integer not null references users(id),
     created datetime not null default current_timestamp,
     last_seen datetime not null default current_timestamp
 ) without rowid;
