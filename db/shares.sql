e44ecc335a63e3b3d26d8d566ab03658da98bf37
Laurențiu Nicola <lnicola@dend.ro>
2020-04-12 21:00:26 +0300

    Add access_level column to shares

--- db/shares.sql
+++ db/shares.sql
@@ -2,5 +2,6 @@ create table shares (
     id integer not null primary key,
     name text not null unique,
     path text not null,
-    access_level int not null
+    access_level int not null,
+    upload_allowed boolean not null
 );
