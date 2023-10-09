feea236835bd3ebba1e5c6e436d3dec4eaa31843
Laurențiu Nicola <lnicola@dend.ro>
2023-10-09 11:32:54 +0300

    Bump axum to 0.6

--- src/authentication.rs
+++ src/authentication.rs
@@ -1,16 +1,15 @@
-use std::pin::Pin;
+use std::convert::Infallible;
 use std::sync::Arc;
 
 use crate::db::SqliteStore;
 use crate::response;
 use crate::RustyShare;
-use axum::body::{self, Empty};
-use axum::extract::{FromRequest, RequestParts};
-use axum::response::{IntoResponse, Response};
+use async_trait::async_trait;
+use axum::extract::{FromRef, FromRequestParts};
+use axum::response::Response;
 use headers::{Cookie, HeaderMapExt};
-use http::StatusCode;
+use http::request::Parts;
 use http::Uri;
-use std::future::Future;
 
 pub enum Authentication {
     User(i32, String),
@@ -43,41 +42,22 @@ fn check_session(
     Ok(r)
 }
 
-pub enum AuthenticationRejection {
-    Db,
-}
+#[async_trait]
+impl<S> FromRequestParts<S> for Authentication
+where
+    Arc<RustyShare>: FromRef<S>,
+    S: Send + Sync,
+{
+    type Rejection = Infallible;
 
-impl IntoResponse for AuthenticationRejection {
-    fn into_response(self) -> Response {
-        match self {
-            AuthenticationRejection::Db => Response::builder()
-                .status(StatusCode::INTERNAL_SERVER_ERROR)
-                .body(body::boxed(Empty::new()))
-                .unwrap(),
-        }
-    }
-}
-
-impl<B: Send> FromRequest<B> for Authentication {
-    type Rejection = AuthenticationRejection;
-    fn from_request<'a, 'f>(
-        req: &'a mut RequestParts<B>,
-    ) -> Pin<Box<dyn Future<Output = Result<Self, Self::Rejection>> + Send + 'f>>
-    where
-        'a: 'f,
-    {
-        Box::pin(async move {
-            let rusty_share = req
-                .extensions()
-                .get::<Arc<RustyShare>>()
-                .ok_or(AuthenticationRejection::Db)?;
-            let store = &rusty_share.store;
-            let cookie = req.headers().typed_get::<Cookie>();
-            let authentication = match check_session(store, req.uri(), cookie) {
-                Ok((user_id, name)) => Authentication::User(user_id, name),
-                Err(response) => Authentication::Error(response),
-            };
-            Ok(authentication)
-        })
+    async fn from_request_parts(parts: &mut Parts, state: &S) -> Result<Self, Self::Rejection> {
+        let rusty_share = Arc::<RustyShare>::from_ref(state);
+        let store = &rusty_share.store;
+        let cookie = parts.headers.typed_get::<Cookie>();
+        let authentication = match check_session(store, &parts.uri, cookie) {
+            Ok((user_id, name)) => Authentication::User(user_id, name),
+            Err(response) => Authentication::Error(response),
+        };
+        Ok(authentication)
     }
 }
